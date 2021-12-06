fee0fcb02c13fab6ba3785ecf32140a59f0ee1ae
KRISHNA-JAIS <krishna.jaiswal@knoldus.com>
2021-12-07 00:47:19 +0530

    first commit

---
+++ src/ReadFileTrans/Main.java
@@ -0,0 +1,38 @@
+package ReadFileTrans;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Main {
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/ReadFileTrans/Read.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        HashMap<String,Integer> map=new HashMap<String,Integer>();
+
+        String st;
+
+        while ((st = br.readLine()) != null) {
+            String[] arr=st.split("\\s");
+            for (String s : arr) {
+                if (!map.containsKey(s)) {
+                    map.put(s, 1);
+                }
+                else {
+                    map.put(s, map.get(s) + 1);
+                }
+            }
+        }
+
+        System.out.println(map);
+
+    }
+}
